ef34c5dfac07e090e41fe4f873d83477afc33cb7
Sahil Verma <domainsahil007@gmail.com>
2024-01-03 01:44:25 +0530

    user watch history route and controllers added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -420,3 +420,59 @@ export const getUserChannelProfile = asyncHandler(
     );
   }
 );
+
+export const getUserHistory = asyncHandler(
+  async (req: Request, res: Response) => {
+    const user = await User.aggregate([
+      {
+        $match: {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          _id: new Types.ObjectId(req?.user?._id),
+        },
+        $lookup: {
+          from: "videos",
+          localField: "watchHistory",
+          foreignField: "_id",
+          as: "watchHistory",
+          pipeline: [
+            {
+              $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                  {
+                    $project: {
+                      fullName: 1,
+                      username: 1,
+                      avatar: 1,
+                    },
+                  },
+                  {
+                    $addFields: {
+                      owner: {
+                        $first: "$owner",
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+    ]);
+
+    if (!user?.length) {
+      throw new ApiError(404, "Failed to fet user watch history Data");
+    }
+
+    res.status(200).json(
+      new ApiResponse(200, "Get Watch History Data", {
+        watchHistory: user[0]?.watchHistory,
+      })
+    );
+  }
+);
--- src/router/user.routes.ts
+++ src/router/user.routes.ts
@@ -2,10 +2,16 @@ import { Router } from "express";
 
 // controller
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getUserHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller";
 
 // middlewares
@@ -32,5 +38,18 @@ router.route("/login").post(loginUser);
 // secured Routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT);
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getUserHistory);
 
 export const userRouter = router;
