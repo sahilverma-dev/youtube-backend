d7e0d4a9a6c5a26499f238a442a66701f382c7b5
Sahil Verma <domainsahil007@gmail.com>
2024-01-03 01:28:52 +0530

    video info route added

--- src/controllers/videos.controller.ts
+++ src/controllers/videos.controller.ts
@@ -6,6 +6,62 @@ import { uploadOnCloudinary } from "../services/cloudnary/config";
 import { Video } from "../models/video.model";
 import { Types } from "mongoose";
 
+export const getVideoInfo = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    if (!id) {
+      throw new ApiError(400, "Video ID is required");
+    }
+
+    const video = await Video.aggregate([
+      {
+        $match: {
+          _id: new Types.ObjectId(id),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          foreignField: "_id",
+          localField: "owner",
+          as: "owner",
+          pipeline: [
+            {
+              $project: {
+                fullName: 1,
+                avatar: 1,
+                username: 1,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $addFields: {
+          owner: {
+            $first: "$owner",
+          },
+        },
+      },
+    ]);
+
+    if (video.length) {
+      res.status(200).json(
+        new ApiResponse(200, "Video found", {
+          video: video[0],
+        })
+      );
+    } else {
+      res.status(404).json(
+        new ApiResponse(404, "Video not found", {
+          video: null,
+        })
+      );
+    }
+  }
+);
+
 export const uploadVideo = asyncHandler(async (req: Request, res: Response) => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
--- src/router/video.routes.ts
+++ src/router/video.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { uploadVideo } from "../controllers/videos.controller";
+
+// middlewares
 import { verifyJWT } from "../middlewares/auth.middleware";
 import { upload } from "../middlewares/multer.middleware";
+
+// controllers
+import { getVideoInfo, uploadVideo } from "../controllers/videos.controller";
 
 const router = Router();
 
 // normal routes
+router.get("/:id", getVideoInfo);
 
 // secured routes
 router.post(
